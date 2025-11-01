2124aa84cc06854209964413fed68c85da4fd4b9
Ruslan <jer_X_2014_projekt@mail.ru>
2025-11-01 14:44:55 +0300

    Order section created

--- js/main.js
+++ js/main.js
@@ -150,4 +150,10 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
+
+  // ================Маска для телефона=====================
+
+  const inputsTel = document.querySelectorAll('input[type="tel"]');
+  let im = new Inputmask("+7 (999) 999-99-99");
+  im.mask(inputsTel);
 });
